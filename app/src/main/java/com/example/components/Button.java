7256b5db28173663a56e3edadcc3b6dc631bc467
leidy <leidyzulu9@gmail.com>
2017-09-06 12:41:24 -0500

    Metodo onclick para los botones, creación de radioGroup

--- app/src/main/java/com/example/components/Button.java
+++ app/src/main/java/com/example/components/Button.java
@@ -1,13 +1,33 @@
 package com.example.components;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Button extends AppCompatActivity {
+
+    private android.widget.Button buttonFirst;
+    private android.widget.Button buttonSecond;
+    private android.widget.Button buttonThird;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        buttonFirst =  (android.widget.Button) findViewById(R.id.button_first);
+        buttonSecond = (android.widget.Button) findViewById(R.id.button_second);
+        buttonThird = (android.widget.Button) findViewById(R.id.button_third);
+    }
+
+    public void buttonSave(View view){
+
+        Intent intent = new Intent(this, RadioButton.class);
+        startActivity(intent);
     }
 }
