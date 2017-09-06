632ab369603b545cc22ea802f75b4831968764af
leidy <leidyzulu9@gmail.com>
2017-09-06 13:21:04 -0500

    Se elimina el tercer componente

--- app/src/main/java/com/example/components/Button.java
+++ app/src/main/java/com/example/components/Button.java
@@ -3,29 +3,48 @@ package com.example.components;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class Button extends AppCompatActivity {
 
     private android.widget.Button buttonFirst;
     private android.widget.Button buttonSecond;
-    private android.widget.Button buttonThird;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
         initComponents();
+        onclickListener();
     }
 
     private void initComponents() {
 
-        buttonFirst =  (android.widget.Button) findViewById(R.id.button_first);
+        buttonFirst = (android.widget.Button) findViewById(R.id.button_first);
         buttonSecond = (android.widget.Button) findViewById(R.id.button_second);
-        buttonThird = (android.widget.Button) findViewById(R.id.button_third);
+
+
     }
 
-    public void buttonSave(View view){
+    private void onclickListener() {
+        buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Button.this, RadioButton.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSecond.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return false;
+            }
+        });
+    }
+
+    public void buttonSave(View view) {
 
         Intent intent = new Intent(this, RadioButton.class);
         startActivity(intent);
