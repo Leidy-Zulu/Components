6589277664a7470aeb918194c14ce784d9886996
leidy <leidyzulu9@gmail.com>
2017-09-07 17:14:06 -0500

    Se crea vista con linearLayout y relativeLayout

--- app/src/main/java/com/example/components/MainActivity.java
+++ app/src/main/java/com/example/components/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.relative_vs_linear);
     }
 
     public void loadSpinner(View view){
